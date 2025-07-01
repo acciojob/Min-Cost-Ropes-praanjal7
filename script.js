ee5c338bac8469270b4a2ec24cf265d010682c36
praanjal7 <praanjalpagar@gmail.com>
2025-07-02 01:18:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
-}
+function mincost(arr) {
+  arr.sort((a, b) => a - b); // Pehle chhoti se badi sort kar lein
+  let cost = 0;
 
-module.exports=mincost;
+  while (arr.length > 1) {
+    let first = arr.shift();  // sabse chhoti rope
+    let second = arr.shift(); // doosri chhoti rope
+
+    let newRope = first + second;
+    cost += newRope;
+
+    arr.push(newRope);        // jodi hui rope daal do
+    arr.sort((a, b) => a - b); // wapas chhoti se badi sort karo
+  }
+
+  return cost;
+}
